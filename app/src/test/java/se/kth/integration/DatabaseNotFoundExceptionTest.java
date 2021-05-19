90336475b6969b2cf863eca61f90395950b5b8c1
Marius S <seffermarius@gmail.com>
2021-05-19 15:12:19 +0200

    more tests written for exceptions

--- app/src/test/java/se/kth/integration/DatabaseNotFoundExceptionTest.java
+++ app/src/test/java/se/kth/integration/DatabaseNotFoundExceptionTest.java
@@ -3,22 +3,35 @@ package se.kth.integration;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import se.kth.model.Item;
 
 /**
  * testing that DatabaseNotFoundException is thrown when it should
  */
 class DatabaseNotFoundExceptionTest {
 
+    @Test
+    public void getTestCheckInventoryJunit3() {
+        ExternalInventorySystem eis = new ExternalInventorySystem();
+        Integer ItemId = 420;
+        Integer wantedAmount = 5;
+
+    }
+
 
     @Test
     public void testCheckInventoryThrowsDatabaseNotFoundException() {
         ExternalInventorySystem eis = new ExternalInventorySystem();
-        Item item = new Item();
         Integer itemId = 420;
         Integer wantedAmount = 2;
-        Assertions.assertThrows(DatabaseFailureException.class, () -> {
+        Throwable exception = Assertions.assertThrows(DatabaseFailureException.class, () -> {
             eis.checkInventory(itemId, wantedAmount);
         });
+        Assertions.assertTrue(exception.getMessage().equals("database connection timed out"));
     }
+
+    @Test
+    public void testCheckInventory
+
+
+
 }
--- app/src/test/java/se/kth/integration/ItemNotFoundExceptionTest.java
+++ app/src/test/java/se/kth/integration/ItemNotFoundExceptionTest.java
@@ -3,21 +3,47 @@ package se.kth.integration;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import se.kth.model.Item;
 
 /**
  * testing that ItemNotFoundException is thrown when it should
  */
 class ItemNotFoundExceptionTest {
 
+    /**
+     * checks that custom exception is thrown when itemId can't be found
+     * checks that the message is correct
+     */
     @Test
     public void testCheckInventoryThrowsItemNotFoundException() {
-        ExternalInventorySystem eis = new ExternalInventorySystem();
-        Item item = new Item();
         Integer itemId = 20;
         Integer wantedAmount = 2;
-        Assertions.assertThrows(ItemNotFoundException.class, () -> {
+        ExternalInventorySystem eis = new ExternalInventorySystem();
+        Throwable exception = Assertions.assertThrows(ItemNotFoundException.class, () -> {
             eis.checkInventory(itemId, wantedAmount);
         });
+        Assertions.assertTrue(exception.getMessage().equals("The item with the itemID "+ itemId + " was not found"));
+    }
+
+    /**
+     * testing that CheckInventory doesn't throw any exceptions during normal execution.
+     * ItemId is existing in inventory system
+     */
+    @Test
+    public void testCheckInventoryNormalExecution() {
+        Integer itemId = 3;
+        Integer wantedAmount = 2;
+        ExternalInventorySystem eis = new ExternalInventorySystem();
+        Assertions.assertDoesNotThrow(() -> eis.checkInventory(itemId, wantedAmount));
+    }
+
+    /**
+     * testing that an exception is thrown when null is given as parameter
+     */
+    @Test
+    public void testCheckInventoryWhenAmountIsNull() {
+        Integer itemId = 3;
+        Integer wantedAmount = null;
+        ExternalInventorySystem eis = new ExternalInventorySystem();
+        Assertions.assertThrows(Exception.class, () -> eis.checkInventory(itemId, wantedAmount));
     }
 }
